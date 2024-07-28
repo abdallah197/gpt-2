4970540b15586f194bb26d15a6daed8eac1928a6
abdallah197 <abdullahbashir077@gmail.com>
2024-07-28 14:09:50 +0100

    added from_pretrianed

--- gpt.py
+++ gpt.py
@@ -1,8 +1,12 @@
 import math
 from dataclasses import dataclass
+
 import torch
 import torch.nn as nn
-ifrom torch.nn import functional as F
+
+ifrom
+torch.nn
+import functional as F
 
 
 @dataclass
@@ -13,13 +17,14 @@ class GPTConfig:
     n_head: int = 6
     n_embed = 384
 
+
 class CausalSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
         assert config.n_embed % config.n_head == 0
         self.n_head = config.n_head
-        self.c_attn =  nn.Linear(config.n_embed, config.n_embed * 3)
+        self.c_attn = nn.Linear(config.n_embed, config.n_embed * 3)
 
         self.c_proj = nn.Linear(config.n_embed, config.n_embed)
         self.dropout = nn.Dropout(config.dropout)
@@ -35,16 +40,16 @@ class CausalSelfAttention(nn.Module):
         k = k.view(B, -1, self.h_embed, self.config.n_embed // self.n_head)
         v = v.view(B, -1, self.h_embed, self.config.n_embed // self.n_head)
 
-
         dk = k.shape[-1]
         # calculate the scores
         scores = (q @ k.transpose(-1, -2)) * (1.0 / math.sqrt(dk))
         scores = scores.masked_fill_(self.bias[:, :, :T, :T] == 0, float('-inf'))
         scores = F.softmax(scores, dim=-1)
 
-        p_scores = scores @ v # B, T, n_h, hd
+        p_scores = scores @ v  # B, T, n_h, hd
         p_scores = p_scores.tranpose(1, 2).contiguous().view(B, T, C)
         return self.c_proj(p_scores)
+
 
 class MLP(nn.Module):
     def __init__(self, config):
@@ -58,6 +63,8 @@ class MLP(nn.Module):
         x = self.gelu(self.c_fc(x))
         x = self.c_proj(x)
         return x
+
+
 class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -71,15 +78,66 @@ class Block(nn.Module):
         x = self.attn(self.ln_1(x)) + x
         x = self.ffn(self.ln_2(x)) + x
         return x
+
+
 class GPT(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
 
         self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embed),
-            wpe = nn.Embedding(config.block_size, config.n_embed),
-            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-            ln_f = nn.LayerNorm(config.n_embed)
+            wte=nn.Embedding(config.vocab_size, config.n_embed),
+            wpe=nn.Embedding(config.block_size, config.n_embed),
+            h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f=nn.LayerNorm(config.n_embed)
         ))
         self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            'gpt2': dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium': dict(n_layer=24, n_head=16, n_embd=1024),  # 350M params
+            'gpt2-large': dict(n_layer=36, n_head=20, n_embd=1280),  # 774M params
+            'gpt2-xl': dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M params
+        }[model_type]
+        config_args['vocab_size'] = 50257  # always 50257 for GPT model checkpoints
+        config_args['block_size'] = 1024  # always 1024 for GPT model checkpoints
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]  # discard this mask / buffer, not a param
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')]  # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')]  # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
